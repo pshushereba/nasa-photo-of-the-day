6fbd9a90d8e431028533ba002da9cffc36e2c4da
Patrick Shushereba <pshushereba@gmail.com>
2019-07-30 20:24:39 -0700

    Started creating components.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
+import Info from "./components/Info.js";
+import Photo from "./components/NASAPhoto.js";
+
+
+
 function App() {
+  
+  const [photo, setPhoto] = useState();
+  // const []
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        .then((response) => {
+          setPhoto(response.data.hdurl);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  }, []);
+  
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Photo photo={photo} />
+      <Info />
     </div>
   );
 }
